176b40c0fc73de0b61a170fba004ba54d6e30f07
Jose Alvarez Vivar <josealvarezvivar@gmail.com>
2021-04-24 11:47:19 +0200

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,111 @@
+var gameObject = {
+    properties: {
+        gameBoard: null,
+        gameMatrix: null,
+        matrixNextState: null,
+    },
+    initGame: function(){
+        let newCell;
+        let cellsWidth = 5;
+        let cellsHeight = 5;
+
+        let totalCells = cellsWidth * cellsHeight;
+        this.properties.gameBoard = document.getElementById("game-board");
+
+        for(var i = 0; i < totalCells; i++){
+            newCell = document.createElement("div");
+            newCell.classList.add("cell");
+            newCell.classList.add("dead");
+            newCell.setAttribute("id", "cell-" + i);
+            this.properties.gameBoard.appendChild(newCell);
+        }
+
+        this.properties.gameMatrix = new Array(cellsWidth);
+        for (var i = 0; i < cellsWidth; i++){
+            
+            this.properties.gameMatrix[i] = new Array(cellsHeight);
+            for(var j = 0; j < cellsHeight; j++){
+                this.properties.gameMatrix[i][j] = 0;
+            }
+        }
+
+        this.properties.matrixNextState = new Array(cellsWidth);
+        for (var i = 0; i < cellsWidth; i++){
+            
+            this.properties.matrixNextState[i] = new Array(cellsHeight);
+            for(var j = 0; j < cellsHeight; j++){
+                this.properties.matrixNextState[i][j] = 0;
+            }
+        }
+    },
+    setAliveCells: function(){
+        cells = [
+            [1,2],
+            [2,2],
+            [3,2],
+        ]
+
+        for(var i = 0; i < cells.length; i++){
+            this.properties.matrixNextState[cells[i][0]][cells[i][1]] = 1;
+        }
+    },
+    manageCells: function(){
+
+        for(let i = 0; i < this.gameObject.properties.gameMatrix.length; i++){
+            for(let j = 0; j < this.gameObject.properties.gameMatrix[i].length; j++){
+
+                let currentCellId = "cell-" + ((this.gameObject.properties.gameMatrix[i].length * i) + j);
+                let currentCell = document.getElementById(currentCellId);
+
+                //1- Paint current state of cells.
+                this.gameObject.properties.gameMatrix[i][j] = this.gameObject.properties.matrixNextState[i][j];
+
+                if(this.gameObject.properties.gameMatrix[i][j] ===  1){
+                    currentCell.classList.remove("dead");
+                    currentCell.classList.add("alive");
+                }else if (this.gameObject.properties.gameMatrix[i][j] === 0){
+                    currentCell.classList.remove("alive");
+                    currentCell.classList.add("dead");
+                }
+            }
+        }
+
+        for(let i = 0; i < this.gameObject.properties.gameMatrix.length; i++){
+            for(let j = 0; j < this.gameObject.properties.gameMatrix[i].length; j++){
+                let numOfNeighbors = 0;
+                let arrayLength = this.gameObject.properties.gameMatrix.length;
+
+                //2- Get neighbors cells.
+                let currentNeighbors = [
+                    this.gameObject.properties.gameMatrix[(i === 0 ? arrayLength : i)-1][(j === 0 ? arrayLength : j)-1],
+                    this.gameObject.properties.gameMatrix[(i === 0 ? arrayLength : i)-1][j],
+                    this.gameObject.properties.gameMatrix[(i === 0 ? arrayLength : i)-1][(j === arrayLength-1 ? 0 : j)+1],
+                    this.gameObject.properties.gameMatrix[i][(j === 0 ? arrayLength : j)-1],
+                    this.gameObject.properties.gameMatrix[i][(j === arrayLength-1 ? 0 : j)+1],
+                    this.gameObject.properties.gameMatrix[(i === arrayLength-1 ? 0 : i)+1][(j === 0 ? arrayLength : j)-1],
+                    this.gameObject.properties.gameMatrix[(i === arrayLength-1 ? 0 : i)+1][j],
+                    this.gameObject.properties.gameMatrix[(i === arrayLength-1 ? 0 : i)+1][(j === arrayLength-1 ? 0 : j)+1],
+                ]
+
+                //3- Set next state for cells.
+                for(let k = 0; k < currentNeighbors.length; k++){
+                    if(currentNeighbors[k] === 1)
+                        numOfNeighbors++;
+                }
+
+                if(this.gameObject.properties.gameMatrix[i][j] === 1){
+                    if(numOfNeighbors <= 1 || numOfNeighbors >= 4)
+                        this.gameObject.properties.matrixNextState[i][j] = 0;
+                    else if (numOfNeighbors === 2 || numOfNeighbors === 3)
+                        this.gameObject.properties.matrixNextState[i][j] = 1;
+                }else if(this.gameObject.properties.gameMatrix[i][j] === 0){
+                    if(numOfNeighbors === 3)
+                        this.gameObject.properties.matrixNextState[i][j] = 1;
+                }
+            }
+        }
+    },
+    play: function(){
+        setInterval(this.manageCells, 1000);
+    }
+}
